8fc5357bb587ff615dffeec87411f19f84723115
tobymannHSO <toby.mann@henryscheinone.co.nz>
2023-06-19 03:26:45 +1200

    Write test helper method to return sample record of model with given attr missing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +10,14 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Returns an example of the model missing the given attr
+  # Initial intention is to easily test validations
+  def model_missing_attr(model, valid_fixture, attr)
+    sample_attrs = valid_fixture.attributes
+    test_record = model.new(sample_attrs)
+    test_record[attr.to_sym] = nil
+
+    test_record
+  end
 end
