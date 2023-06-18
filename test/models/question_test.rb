98993ec70ab1321205537c1c67c0bab73501fa28
tobymannHSO <toby.mann@henryscheinone.co.nz>
2023-06-19 02:20:49 +1200

    formatting changes to questions model test file.

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,24 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  test "Can be saved" do
+  test 'Can be saved' do
     assert Question.create
   end
 
-  test "validates presence of header" do
+  test 'validates presence of header' do
     qn = Question.new
     assert qn.invalid?
 
-    qn.header = "header"
+    qn.header = 'header'
     assert qn.valid?
   end
 
-  test "has_many answers" do
+  test 'has_many answers' do
     qn = questions(:one)
     assert qn.answers
   end
 
-  test "answers are destroyed with question" do
+  test 'answers are destroyed with question' do
     qn = questions(:one)
     assert_difference('Answer.count', -1) { qn.destroy }
   end
